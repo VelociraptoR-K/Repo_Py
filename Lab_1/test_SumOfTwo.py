6abd1845fec044c747cb20d9eaf3383f6dcea603
Nikolay <nikybys@gmail.com>
2025-09-27 03:27:46 +0300

    Update test_SumOfTwo.py

--- Lab_1/test_SumOfTwo.py
+++ Lab_1/test_SumOfTwo.py
@@ -16,19 +16,24 @@ class Tests(unittest.TestCase):
 
     def test_type_target_string(self):
         self.assertEqual(sum_two([1,2], '9.76'), 'Переменная target строковая, а должна быть целочисленной')
-
+        
+    # Проверка на работоспособность с отрицательными числами
     def test_NegativeValue(self):
         self.assertEqual(sum_two([2,5,1,-3], 2),[1,3])
-
+        
+    # Проверка на работоспособность с одинаковыми значениями в списке
     def test_SameValues(self):
         self.assertEqual(sum_two([4,4,4,4], 8),[0,1])
-
+        
+    # Стандартная проверка, на соответствие ожидаемого вывода с реальным
     def test_Equal(self):
         self.assertEqual(sum_two([2,7,11,15], 9),[0,1])
-
+        
+    # Тест на случай возможного отсутствия target в списке
     def test_Is_In(self):
         self.assertEqual(sum_two([2,7,11,15], 99),'В списке не нашлось нужной комбинации')
 
 if __name__ == '__main__':
 
     unittest.main()
+
