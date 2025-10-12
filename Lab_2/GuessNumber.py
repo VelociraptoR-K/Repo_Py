e2719d8ea0d042047c3e471d1919e6242f5dce15
Nikolay <nikybys@gmail.com>
2025-10-12 12:08:55 +0300

    Update GuessNumber.py

--- Lab_2/GuessNumber.py
+++ Lab_2/GuessNumber.py
@@ -14,28 +14,47 @@ def main():
     __вывод результатов на экран__
     :return:
     """
-
     target = int(input('Введите target: '))
-    diap = sorted(list(map(int,input('Введите числа интервала через пробел: ').split())))
-    print(f'Ваш отсортированный диапазон: {diap}')
-    res = guess_number(target, diap, type='bin')
+    diap = sorted(list(map(int,input('Введите числа диапазона через пробел: ').split())))
+    print(f'Ваш диапазон после сортировки: {diap}')
+    res = guess_number(target, diap,search_type='bin')
     print(f'{res}')
 
-
-def guess_number(target, lst, type='seq') -> list[int, int | None]:
-    if type == 'seq':
+def guess_number(target: int, lst:list, search_type: str ='seq') -> list[int, int | None]:
+    if type(lst) != list:
+        return 'На вход подан не список'
+    try:
+        target_int = int(target)
+        if target_int != float(target):  # Проверяем, что число целое
+            return 'На вход подано нецелое число'
+    except (ValueError, TypeError):
+        return 'Данную строку нельзя преобразовать к int'
+    target = target_int
+    if target != int(target):
+        return 'На вход подано нецелое число'
+    if lst == []:
+        return 'На вход подан пустой список'
+    if search_type != 'seq' and search_type != 'bin':
+        return 'Некорректный ввод типа поиска.'
+    if search_type == 'seq':
         # ищем число последовательно
         for i in range(len(lst)):
             if lst[i] == target:
                 return [target,i+1]
-    elif type == 'bin':
+    elif search_type == 'bin':
         # ищем число с помощью алгоритма бинарного поиска
-        mid = len(lst)//2
-        if lst[mid] == target:
-            return [target,1]
-        elif lst[mid] < target:
-            mid =
+        srez = lst[:]
+        c = 0
+        while srez:  # пока список не пустой
+            c += 1
+            mid = len(srez) // 2
+            if srez[mid] == target:
+                return [target, c]
+            elif srez[mid] < target:
+                srez = srez[mid:] # в большую сторону (правая половина)
+            else:
+                srez = srez[:mid] # в меньшую сторону (левая половина)
+    return [target, None]
 
 if __name__ == '__main__':
-    pass
     main()
