b528b4ed19b20bc228aa657451056eed7137bfa2
Nikolay <nikybys@gmail.com>
2025-10-31 15:38:11 +0300

    Update BinaryTree.py

--- Lab_3/BinaryTree.py
+++ Lab_3/BinaryTree.py
@@ -29,12 +29,13 @@ def gen_bin_tree_tuple(height:int = 4, root:int = 3, l_b:int=left_branch, r_b:in
     if height < 0:
         return 'Вершина дерева должна быть >= 0'
     if height == 0:
-        return (root,[])
+        return f"({root}: [])"
     left_b = l_b(root)
     right_b = r_b(root)
     left = gen_bin_tree_tuple(height - 1, left_b, l_b, r_b)
     right = gen_bin_tree_tuple(height - 1, right_b, l_b, r_b)
-    return (root, [left, right])
+    return f"({root}: [{left}, {right}])"
+
 
 
 
